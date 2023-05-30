373a17e3cc380676325a49cfaa1ceecb75bee922
Fedor Metsger <fedor.metsger@gmail.com>
2023-05-30 17:05:45 +0300

    Move to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 
-from catalog.views import index, contacts, product
+from catalog.views import contacts, ProductDetailView, ProductListView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ProductListView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', product, name='product')
+    # path('product/<int:pk>/', product, name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,21 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from catalog.models import Contact, Product
 
+class ProductListView(ListView):
+    model = Product
+    extra_context = {
+        'title': 'Главная страница'
+    }
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by('-creation_date')[:5]
+        return queryset
 
-# Create your views here.
-def index(request):
-    # context = {"products": []}
-    # for p in Product.objects.order_by('-creation_date')[:5]:
-    #     context["products"].append({"name": p.name, "category": p.category,
-    #                                 "price": p.price, "date": p.creation_date})
-    context = {"products": Product.objects.order_by('-creation_date')[:5]}
-    return render(request, "index.html", context=context)
+class ProductDetailView(DetailView):
+    model = Product
 
 def contacts(request):
     if request.method == 'POST':
@@ -28,18 +31,3 @@ def contacts(request):
         context["contacts"].append({"name": c.name, "phone": c.phone, "email": c.email})
 
     return render(request, "contacts.html", context=context)
-
-def product(request, pk):
-    # if request.method == 'POST':
-    #     name = request.POST.get('name')
-    #     phone = request.POST.get('phone')
-    #     email = request.POST.get('email')
-    #     print(f'Получена контактная информация: {name}, тел.: {phone}, e-mail: {email}')
-    #     if name and (phone or email):
-    #         Contact.objects.create(name=name, phone=phone, email=email)
-
-    context = {
-        "product": Product.objects.filter(pk=pk)[0]
-    }
-
-    return render(request, "product.html", context=context)
