e0bd64c5e0e74f339b5d66a22f1ccd649b3bcbfd
Fedor Metsger <fedor.metsger@gmail.com>
2023-06-29 21:47:49 +0300

    Add homework #9 solution

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.views import contacts, ProductDetailView
 from catalog.views import ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView
@@ -9,7 +10,7 @@ app_name = 'catalog'
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('product/create/', ProductCreateView.as_view(), name='product_create')
--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -82,14 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": "localhost",
-        "NAME": "catalog",
-        "PORT": 5432,
-        "USER": "postgres",
-        "PASSWORD": "postgres",
+        "NAME": os.getenv("DB_NAME"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD")
     }
 }
 
@@ -152,3 +153,12 @@ EMAIL_HOST_PASSWORD = "AsTSNVv7pun9"
 EMAIL_USE_SSL = True
 
 ROLEPERMISSIONS_MODULE = 'config.roles'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': os.getenv("REDIS_HOST")
+    }
+}
+
+LOW_CACHED = (os.getenv('LOW_CACHED', 'False') == 'True')
